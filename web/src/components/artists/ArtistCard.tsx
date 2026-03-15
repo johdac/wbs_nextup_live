6f6b9f66072da910fbb2238a26bd786588c250f0
mingshen-yen <minghsuanyen@gmail.com>
2026-03-15 14:31:39 +0100

    clean code

--- web/src/components/artists/ArtistCard.tsx
+++ web/src/components/artists/ArtistCard.tsx
@@ -2,16 +2,11 @@ import { Link } from "react-router";
 import DOMPurify from "dompurify";
 import type { Artist } from "../../services/artistsApi";
 import { GenresTag } from "../ui/GenresTag";
-import { PlayBtn } from "../buttons/PlayBtn";
-import { AddToListBtn } from "../buttons/AddToListBtn";
 
 export const ArtistCard = ({ artist }: { artist: Artist }) => {
   return (
     <>
-      <div
-        key={artist.id}
-        className="grid md:grid-cols-3 py-3 rounded-lg mb-2 gap-5"
-      >
+      <div key={artist.id} className="grid md:grid-cols-3 py-3 rounded-lg mb-2 gap-5">
         <img
           src={artist.mainImageUrl}
           alt={artist.name}
@@ -24,9 +19,7 @@ export const ArtistCard = ({ artist }: { artist: Artist }) => {
         <div className="flex gap-2">
           <div className="gap-1">
             <Link to={`/artist/${artist.id}`}>
-              <h3 className="transition-colors duration-100 hover:text-purple cursor-pointer">
-                {artist.name}
-              </h3>
+              <h3 className="transition-colors duration-100 hover:text-purple cursor-pointer">{artist.name}</h3>
               <div
                 className="text-base text-gray line-clamp-2"
                 dangerouslySetInnerHTML={{
